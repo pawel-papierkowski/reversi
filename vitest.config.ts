b0bd698fa6dfb3e3e53ec82e8bd7f5c9c0e4c31b
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-03-09 18:03:25 +0100

    Tests now should work both in terminal and in IDE.

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import angular from '@analogjs/vite-plugin-angular';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    angular(),
+    tsconfigPaths()
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+    include: ['src/**/*.spec.ts'],
+  }
+});
