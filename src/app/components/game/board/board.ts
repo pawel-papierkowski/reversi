cfb9bba94885adbe0157c9ae6dbe56ccdae8270c
Mader Levap <mader.levap@gmail.com>
2026-03-18 13:56:20 +0100

    Implement tinted board for end of round or history.

--- src/app/components/game/board/board.ts
+++ src/app/components/game/board/board.ts
@@ -3,6 +3,7 @@ import { Component, inject, computed } from '@angular/core';
 import { GameStateService } from '@/code/services/gameState/gameState.service';
 
 import { ReversiCellCmp } from '@/components/game/cell/cell';
+import { EnGameStatus, EnViewMode } from '@/code/data/enums';
 
 @Component({
   selector: 'reversi-board',
@@ -19,6 +20,20 @@ export class ReversiBoardCmp {
   // Computed signal to generate letters: ['a', 'b', 'c' ...]
   public readonly columns = computed(() => {
     const size = this.gameStateService.gameState().settings.boardSize;
-    return Array.from({ length: size }, (_, i) => String.fromCharCode(97 + i)); // 97 is ASCII 'a'
+    // 97 is ASCII for 'a'
+    return Array.from({ length: size }, (_, i) => String.fromCharCode(97 + i));
+  });
+
+  public readonly boardCss = computed(() => {
+    const isHistory = this.gameStateService.gameState().view.viewMode === EnViewMode.History;
+    const status = this.gameStateService.gameState().board.status;
+    const isEndRound = status === EnGameStatus.PlayerWon || status === EnGameStatus.Tie;
+
+    const cssClasses: string[] = [];
+    cssClasses.push('board');
+    if (isHistory) cssClasses.push('history');
+    else if (isEndRound) cssClasses.push('endRound');
+    console.info(cssClasses);
+    return cssClasses;
   });
 }
