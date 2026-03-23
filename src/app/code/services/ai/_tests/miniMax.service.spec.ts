ed8f76e7477d031e6717e461b173d46334906a1c
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-03-23 17:18:59 +0100

    Additional unit tests for MiniMax.

--- src/app/code/services/ai/_tests/miniMax.service.spec.ts
+++ src/app/code/services/ai/_tests/miniMax.service.spec.ts
@@ -154,13 +154,33 @@ describe('MiniMaxService', () => {
   //
 
   describe('scoring results for specific state of board', () => {
+    it('asymmetric starting point', () => {
+      gameStateService.menuSettings().boardSize = 6; // 6x6, moves: e4 e5 e6 f6 b3 b2 c2 b4 a4
+      gameService.startGame();
+      const gameState = gameStateService.gameState();
+      const boardStr = "______"+
+                       "_WB___"+
+                       "_BBB__"+
+                       "BBBBB_"+
+                       "____W_"+
+                       "____BW";
+      setBoard(gameState, boardStr);
 
-    // TODO additional tests:
-    // - different, asymmetric starting point
-    // - proper handling of endgame:
-    //   - board filled up
-    //   - double pass when board still have empty cells
-
+      const req: MiniMaxReq = {
+        piece: EnCellState.W,
+        maxDepth: 2,
+        legalMoves: legalMoveService.resolveMovesCustom(gameState.board.cells, EnCellState.W),
+        cells: gameState.board.cells,
+      }
+      const actualResponse = miniMaxService.resolve(req);
+      const expectedResponse: MiniMaxResp = { results: [
+        {score: 4, depth: 2, moves: [{x:3, y:1},{x:2, y:0},{x:4, y:2}]},
+        {score: 4, depth: 2, moves: [{x:4, y:2},{x:5, y:2},{x:3, y:1}]},
+        {score: -14, depth: 2, moves: [{x:3, y:5},{x:0, y:0},{x:4, y:2}]},
+        {score: -40, depth: 2, moves: [{x:1, y:4},{x:0, y:2},{x:5, y:3}]},
+      ]};
+      expect(actualResponse, 'Response should be same').toEqual(expectedResponse);
+    });
 
     it('proper handling of single pass', () => {
       gameStateService.menuSettings().boardSize = 4; // 4x4, moves: b1 c1 d3
@@ -187,7 +207,6 @@ describe('MiniMaxService', () => {
       expect(actualResponse, 'Response should be same').toEqual(expectedResponse);
     });
 
-
     it('double pass while there are still empty cells', () => {
       gameStateService.menuSettings().boardSize = 4; // 4x4, moves: c4 d4 d3 b4 a4 d2 d1 a2 pass
       gameService.startGame();
@@ -212,6 +231,29 @@ describe('MiniMaxService', () => {
       expect(actualResponse, 'Response should be same').toEqual(expectedResponse);
     });
   });
+
+    it('board almost completely filled up', () => {
+      gameStateService.menuSettings().boardSize = 4; // 4x4, moves: b1 a1 a2 a3 a4 c4 d4 d3 d2 c1 d1
+      gameService.startGame();
+      const gameState = gameStateService.gameState();
+      const boardStr = "WWWB"+ // white b4 will end the game
+                       "WWBB"+
+                       "WBWB"+
+                       "B_WB";
+      setBoard(gameState, boardStr);
+
+      const req: MiniMaxReq = {
+        piece: EnCellState.W,
+        maxDepth: 5,
+        legalMoves: legalMoveService.resolveMovesCustom(gameState.board.cells, EnCellState.W),
+        cells: gameState.board.cells,
+      }
+      const actualResponse = miniMaxService.resolve(req);
+      const expectedResponse: MiniMaxResp = { results: [
+        {score: -380, depth: 0, moves: [{x:1, y:3}]}, // end of game
+      ]};
+      expect(actualResponse, 'Response should be same').toEqual(expectedResponse);
+    });
 
   // TODO: testing for high difficulty level:
   // - scoring system change when board is mostly filled (over 3/4 of cells not empty)
