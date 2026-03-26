6a1271053cc6e674825b39cdaf864908261e7edf
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-03-26 20:36:03 +0100

    ComboBoxCmp can now have individual minWidth set.

--- src/app/components/basic/comboBox/comboBox.ts
+++ src/app/components/basic/comboBox/comboBox.ts
@@ -6,10 +6,18 @@ import {TranslatePipe} from '@ngx-translate/core';
  * ComboBox component.
  * Serves as replacement for select and option elements since these cannot be styled properly.
  * Variable options expects array of strings or numbers. selectedOption should be value to get/set.
+ * Attributes:
+ * - options: Array of strings or numbers.
+ * - placeholder: Shown if nothing is selected.
+ * - langPrefix: Use it together with to create text string used in translation.
+ * - disabled: If true, combobox is disabled.
+ * - minWidth: If present, sets custom minimum width, otherwise will use defaults. Example: "250px"
+ *
  * Features:
- * - Can choose multiple options.
+ * - Can choose one option from multiple options (basic feature and entire point of combobox).
  * - Can be disabled.
  * - Integration with translate pipeline.
+ * - Certain parameters are customizable, like minimum width.
  */
 @Component({
   selector: 'combo-box',
@@ -18,13 +26,16 @@ import {TranslatePipe} from '@ngx-translate/core';
   styleUrl: './comboBox.css'
 })
 export class ComboBoxCmp {
+  // attributes
   options = input.required<string[]|number[]>();
   placeholder = input('comboBox.placeholder'); // if nothing is selected, this will be shown
   langPrefix = input('');
   disabled = input(false);
+  minWidth = input<string|null>(null); // optional min-width for combobox, if null browser will fall back to the styles defined in CSS files
 
+  // other
   isOpen = signal(false); // indicates visibility of drawdown list of options
-  arrowClass = computed(() => ({ open: this.isOpen() }));
+  arrowClass = computed(() => ({ open: this.isOpen() })); // for arrow animation
   selectedOption = model<string|number|null>(null); // null means nothing is selected yet
 
   constructor() {
